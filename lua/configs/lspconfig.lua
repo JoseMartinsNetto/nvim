3b3ffe8d4c3bb5a2c775c9739430a43be44e2476
José Martins <j.msantos.netto@gmail.com>
2025-02-12 15:18:52 -0300

    Initial commit

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,38 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+
+local lspconfig = require "lspconfig"
+
+-- EXAMPLE
+local servers = { "html", "cssls", "ts_ls", "lua_ls" }
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+if vim.fn.executable "gleam" == 1 then
+  lspconfig.gleam.setup {
+    cmd = { "gleam", "lsp" },
+    filetypes = { "gleam" },
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+
+    root_dir = function(fname)
+      return lspconfig.util.root_pattern "gleam.toml"(fname) or lspconfig.util.find_git_ancestor(fname)
+    end,
+  }
+end
+
+-- configuring single server, example: typescript
+-- lspconfig.ts_ls.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
---
+++ lua/options.lua
@@ -0,0 +1,7 @@
+require "nvchad.options"
+
+local o = vim.o
+
+o.cursorline = true
+o.clipboard = "unnamedplus"
+o.relativenumber = true
